5a5ef01d4be08f5fbc730f10f5e6af2823d84265
garymsx <keijiro.im@gmail.com>
2019-11-04 22:53:07 +0900

    rename bat for YS2HDD

---
+++ YS2/MGSDATA/ForYS2HDD.bat
@@ -0,0 +1,32 @@
+mkdir YS2HDD
+cd YS2HDD
+copy ..\ys2g_01.mgs    TTLMS2.MGS
+copy ..\ys2g_02.mgs    TTLMS3.MGS
+copy ..\ys2g_03.mgs    Y01MUS-0.MGS
+copy ..\ys2g_04.mgs    SHPMUS-3.MGS
+copy ..\ys2g_05.mgs    SHPMUS-0.MGS
+copy ..\ys2g_06.mgs    SHPMUS-2.MGS
+copy ..\ys2g_07.mgs    Y00MUS-0.MGS
+copy ..\ys2g_08.mgs    Y02MUS-0.MGS
+copy ..\ys2g_09.mgs    Y00MUS-3.MGS
+copy ..\ys2g_09.mgs    Y02MUS-3.MGS
+copy ..\ys2g_10.mgs    Y03MUS-0.MGS
+copy ..\ys2g_11.mgs    ZDKMUS-0.MGS
+copy ..\ys2g_13.mgs    Y05MUS-0.MGS
+copy ..\ys2g_14.mgs    Y05MUS-3.MGS
+copy ..\ys2g_15.mgs    Y07MUS-0.MGS
+copy ..\ys2g_16.mgs    Y11MUS-0.MGS
+copy ..\ys2g_17.mgs    Y12MUS-0.MGS
+copy ..\ys2g_18.mgs    Y22MUS-0.MGS
+copy ..\ys2g_19.mgs    Y25MUS-3.MGS
+copy ..\ys2g_20.mgs    Y25MUS-0.MGS
+copy ..\ys2g_21.mgs    Y27MUS-0.MGS
+copy ..\ys2g_21.mgs    Z27MUS-0.MGS
+copy ..\ys2g_22.mgs    Y27MUS-4.MGS
+copy ..\ys2g_22.mgs    Z27MUS-4.MGS
+copy ..\ys2g_23.mgs    Y27MUS-3.MGS
+copy ..\ys2g_24.mgs    ZBSMUS-0.MGS
+copy ..\ys2g_26.mgs    Z22MUS-0.MGS
+copy ..\ys2g_27.mgs    ENDMS1.MGS
+copy ..\ys2g_28.mgs    GOVMUS-0.MGS
+copy ..\ys2g_29.mgs    NWITEM.MGS
